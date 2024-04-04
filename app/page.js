41d69f2c31a064454bdba1c23740dc38ab2e5357
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-04-04 21:33:22 +0200

    Removed eye symmetry

--- app/page.js
+++ app/page.js
@@ -46,12 +46,12 @@ function Page() {
         <Image src={Monster} alt="Monster background" />
         <div>
           <Image
-            className="eye absolute w-[47%] bottom-[21%] right-[52%]"
+            className="eye absolute w-[50%] bottom-[20%] right-[52%]"
             src={MonsterEyeLeft}
             alt="Monster eye left"
           />
           <Image
-            className="eye absolute w-[47%] bottom-[21%] left-[55%]"
+            className="eye absolute w-[49%] bottom-[20%] left-[55%]"
             src={MonsterEyeRight}
             alt="Monster eye right"
           />
