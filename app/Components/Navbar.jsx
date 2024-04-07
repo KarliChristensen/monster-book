c8baafb4f9d336ac4d6891626a521a8c2976a357
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-04-07 22:31:11 +0200

    Replace Navbar text links with image links and add divider line; update Book section with heading

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -2,25 +2,46 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Titel from "../../public/Titel.png";
+import Bogen from "../../public/images/Bogen.png";
+import Forfatterne from "../../public/images/Forfatterne.png";
+import Line from "../../public/images/Streg1.png";
+import Line2 from "../../public/images/Streg2.png";
 
 function Navbar() {
   return (
-    <header className="fixed top-0 bg-transparent w-full h-20 flex flex-row items-center">
-      <Image className="absolute w-20 p-3" src={Titel} alt="Titel image" />
-      <ul className="w-full flex flex-row justify-center items-center space-x-2">
-        <Link
-          className="hover:scale-105 transition-all ease-in-out font-extrabold "
-          href="#book"
-        >
-          Bogen
-        </Link>
-        <Link
-          className="hover:scale-105 transition-all ease-in-out font-extrabold "
-          href="#contact"
-        >
-          Forfatterne
-        </Link>
-      </ul>
+    <header className="fixed top-0 bg-transparent w-full">
+      <div className="relative flex flex-row items-center h-20">
+        <Image
+          className="absolute w-20 p-5 md:p-3"
+          src={Titel}
+          alt="Titel image"
+        />
+        <ul className="w-full flex flex-row justify-center items-center space-x-1 md:space-x-4">
+          <li className="hover:rotate-3 transform transition-all ease-in-out duration-300">
+            <Link href="#book">
+              <Image
+                className="w-20 md:w-24"
+                src={Bogen}
+                alt="Text image - Bogen"
+              />
+            </Link>
+          </li>
+          <li className="hover:rotate-3 transform transition-all ease-in-out duration-300">
+            <Link href="#contact">
+              <Image
+                className="w-36 md:w-48"
+                src={Forfatterne}
+                alt="Text image - Forfatterne"
+              />
+            </Link>
+          </li>
+        </ul>
+        <Image
+          className="absolute transform -translate-x-[2%] translate-y-[50%] max-w-[3500px] h-20 -z-10"
+          src={Line}
+          alt="Divider line for the navbar"
+        />
+      </div>
     </header>
   );
 }
--- app/Components/Sections/Book.jsx
+++ app/Components/Sections/Book.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Book() {
   return (
-    <div id='book' className='w-full h-full'>Book</div>
+    <div id='book' className='w-full h-full snap-start'>
+      <h1 className="text-5xl font-bold text-center mt-20">Book</h1>
+    </div>
   )
 }
 
