fbc857cb990c9a61c3bd932fc773047d493cb65c
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-04-10 00:00:35 +0200

    Add meta tags for search engines, Facebook, and Twitter

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,41 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta property="og:image" content={metadata.images[0].url} />
+        {/* Google / Search Engine Tags */}
+        <meta itemprop="name" content="Lille Monster" />
+        <meta
+          itemprop="description"
+          content="A lille hjemmeside om en lille bog, om et lille monster."
+        />
+        <meta
+          itemprop="image"
+          content="https://i.ibb.co/09kK8DP/Thumbnail.png"
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://monster-taupe.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Lille Monster" />
+        <meta
+          property="og:description"
+          content="A lille hjemmeside om en lille bog, om et lille monster."
+        />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/09kK8DP/Thumbnail.png"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Lille Monster" />
+        <meta
+          name="twitter:description"
+          content="A lille hjemmeside om et lille bog, om et lille monster."
+        />
+        <meta
+          name="twitter:image"
+          content="https://i.ibb.co/09kK8DP/Thumbnail.png"
+        />
       </Head>
       <body className={inter.className}>
         <Navbar />
