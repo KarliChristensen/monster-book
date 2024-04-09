439817eab19461fcae9ca36ca96145df3426de34
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-04-10 00:46:47 +0200

    Different thumbnail approach

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
-import { ImageResponse } from "next/og";
 import Navbar from "./Components/Navbar";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,8 +18,23 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const thumbnail = metadata.images[0];
+
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={thumbnail.url} />
+        <meta property="og:image:width" content={thumbnail.width} />
+        <meta property="og:image:height" content={thumbnail.height} />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={thumbnail.url} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body className={inter.className}>
         <Navbar />
         {children}
