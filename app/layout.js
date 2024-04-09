74b1c9e232b52f69d60405a9c3e7ba12f2c34010
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-04-10 00:43:44 +0200

    ImageResponse abandoned

--- app/layout.js
+++ app/layout.js
@@ -5,52 +5,7 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const runtime = "edge";
-
-export const alt = "Lille Monster";
-export const size = {
-  width: 1200,
-  height: 630,
-};
-
-export const contentType = "image/png";
-
-export async function Image() {
-  const interSemiBold = fetch(
-    new URL("../public/Thumbnail.png", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 128,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        Lille Monster
-      </div>
-    ),
-    {
-      ...size,
-      fonts: [
-        {
-          name: "Inter",
-          data: await interSemiBold,
-          style: "normal",
-          weight: 400,
-        },
-      ],
-    }
-  );
-}
-
-/* export const metadata = {
+export const metadata = {
   title: "Lille Monster",
   description: "A lille hjemmeside om en lille bog, om et lille monster.",
   images: [
@@ -60,7 +15,7 @@ export async function Image() {
       height: 630,
     },
   ],
-}; */
+};
 
 export default function RootLayout({ children }) {
   return (
