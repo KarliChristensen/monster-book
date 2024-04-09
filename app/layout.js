8139bbe9eef6929dcac92d61e28b96ebf91448ac
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-04-09 17:09:08 +0200

    "Added ImageResponse import from next/og and updated import statement for Inter font"

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { ImageResponse } from 'next/og';
 import Navbar from "./Components/Navbar";
 import Image from "next/image";
 import "./globals.css";
