35b1f3de7f37bb53d8f9219715de968b6bb1e13b
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-04-09 23:53:03 +0200

    Thumbnail

--- app/layout.js
+++ app/layout.js
@@ -1,43 +1,20 @@
 import { Inter } from "next/font/google";
-import { ImageResponse } from 'next/og';
 import Navbar from "./Components/Navbar";
-import Image from "next/image";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Monster",
-  description: "A project about the children's book Monster",
-};
-
-export async function GET() {
-  const imageData = await fetch(
-    new URL("../public/images/Thumbnail.png", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: "flex",
-          background: "#f6f6f6",
-          width: "100%",
-          height: "100%",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Image width="256" height="256" src={imageData} alt="Monster" />
-      </div>
-    ),
+  title: "Lille Monster",
+  description: "A lille hjemmeside om en, om et lille monster.",
+  images: [
     {
+      url: "https://i.ibb.co/09kK8DP/Thumbnail.png",
       width: 1200,
       height: 630,
-    }
-  );
-}
+    },
+  ],
+};
 
 export default function RootLayout({ children }) {
   return (
